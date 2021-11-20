a1330a9509d9a1e20aae4dec08b9c53e57c01e81
Emma Secrest <esecrest908@gmail.com>
2021-11-20 15:59:11 -0500

    added module examples along with packaged examples

---
+++ modulesExamples.py
@@ -0,0 +1,13 @@
+# might use modules to organize sections of code into smaller manageable chunks
+import random #importing a module
+
+from random import randint #importing a function from a particular module
+# with this you wont have to prefix it
+
+import math as m #importing a module with an alias
+#give shorter name to save you some typing
+print(m.pi)
+
+#any file that ends in .py is a module -> just referring to another script file
+import ArgsExample #this is a script file that I created earlier
+
---
+++ packagesExamples.py
@@ -0,0 +1,11 @@
+#to install it is pip install name_of_package
+# a packatge is a simple directory having collections of modules in it
+# a package must contain __init__.py
+
+#working with a built in package
+from random import randint
+print(randint(10,20))
+print(randint(10,20))
+print(randint(10,20))
+print(randint(10,20))
+#will result in 4 random numbers each time
